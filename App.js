096b58337689772bfeb4289aef0f056d8fd2a683
HamnaSiddiqui <hamnas200@gmail.com>
2023-06-16 11:59:47 +0500

    react navigation options adding

--- App.js
+++ App.js
@@ -11,9 +11,26 @@ const Stack = createNativeStackNavigator();
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="MealsCategories" component={CategoriesScreen} />
-        <Stack.Screen name="MealsOverview" component={MealsOverviewScreen} />
+      <Stack.Navigator
+        screenOptions={{
+          headerStyle: { backgroundColor: "#202123" },
+          headerTintColor: "white",
+          contentStyle: { backgroundColor: "#343541" },
+        }}
+      >
+        <Stack.Screen
+          name="MealsCategories"
+          component={CategoriesScreen}
+          options={{
+            title: "Hello Hamna",
+          }}
+        />
+        <Stack.Screen
+          name="MealsOverview"
+          component={MealsOverviewScreen}
+          // options={({ route, navigation }) => {}}
+          //route and navigation ae built in given by react native for dynamic setting ofoptions
+        />
       </Stack.Navigator>
     </NavigationContainer>
   );
